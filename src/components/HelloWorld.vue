a5f8aa4fff519ced340e9ea288ea9cd65352cfd0
randitorizkysindata <r.andito@yahoo.com>
2020-03-05 16:05:08 +0800

    no message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,9 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
       </v-col>
     </v-row>
-    <div class="my-2">
-      <v-btn small @click="submit" color="primary">Log-out</v-btn>
-    </div>
   </v-container>
 </template>
 
@@ -22,13 +14,6 @@ import Vue from "vue";
 export default Vue.extend({
   name: "HelloWorld",
 
-  data: () => ({}),
-
-  methods: {
-    submit() {
-      localStorage.removeItem("login");
-      this.$router.push("/");
-    }
-  }
+  data: () => ({})
 });
 </script>
