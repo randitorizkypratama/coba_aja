f44fc7d3c49024d736d845c4cc39bcb2b159de15
syamsul <cybanjar@gmail.com>
2020-03-09 17:06:53 +0800

    Main Screen

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,12 @@
 <template>
   <v-container>
-    <!-- <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" /> -->
-
     <v-row>
       <v-col cols="6" md="2">
         <v-card class="pa-2" max-width="160" hover outlined>
           <v-img
             max-width="80"
             :src="require('../assets/main-screen/FO-Reception.svg')"
-            class="mb-3 ml-auto mr-auto"
+            class="mb-3 ml-auto mr-auto hover"
             width="80"
             height="60"
             contain
@@ -79,6 +77,188 @@
             height="60"
           />
           <p class="text-no-wrap mx-auto">Point Of Sales</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/Banquet.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Banquet</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/Outlet.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Outlet</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/AR.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Accounts Receivables</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/Purchasing.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Purchasing</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/Inventory.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Inventory</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/AP.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Accounts Payables</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/GC.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">General Cashier</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/GL.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">General Ledger</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/FixAssets.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Fixed Assets</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/Engineering.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Engineering</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/SettingTools.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">System Tools</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/Setting.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Setting</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/NightAudit.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Night Audit</p>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="2">
+        <v-card class="pa-2" max-width="160" hover outlined>
+          <v-img
+            max-width="80"
+            :src="require('../assets/main-screen/IncomeAudit.svg')"
+            class="mb-3 ml-auto mr-auto"
+            width="80"
+            contain
+            height="60"
+          />
+          <p class="text-no-wrap mx-auto">Income Audit</p>
         </v-card>
       </v-col>
     </v-row>
