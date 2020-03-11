46cccdbe62b7ad56e42f2518a6487096138fb567
randitorizkysindata <r.andito@yahoo.com>
2020-03-11 11:07:35 +0800

    no message

--- src/vhp-modules/inventory/reports/slow-moving-stock-on-hand/Slow-Moving-Stock-On-Hand.vue
+++ src/vhp-modules/inventory/reports/slow-moving-stock-on-hand/Slow-Moving-Stock-On-Hand.vue
@@ -4,16 +4,28 @@
     <v-container fluid>
       <v-row>
         <v-col cols="6" md="3">
-          <v-select v-model="select" :items="items" item-value="value" label="Language" outlined></v-select>
-          <v-btn color="primary" block depressed large>
+          <v-select
+            v-model="select"
+            :items="mainGroup"
+            item-value="value"
+            label="Main Group"
+            outlined
+            solo-inverted
+          ></v-select>
+          <v-select
+            v-model="select"
+            :items="storeNumber"
+            item-value="value"
+            label="Store Number"
+            outlined
+            solo-inverted
+          ></v-select>
+          <v-text-field label="Days" solo-inverted outlined></v-text-field>
+          <v-btn color="primary" block depressed small>
             <v-icon right dark>mdi-magnify</v-icon>Rounded Button
           </v-btn>
 
           <v-spacer></v-spacer>
-
-          <div>
-            <p class="dashed">A dashed border.</p>
-          </div>
         </v-col>
 
         <v-col cols="12" md="9">
@@ -39,7 +51,21 @@ export default {
     NavBar
   },
   data: () => ({
-    kosong: ""
+    mainGroup: [],
+    storeNumber: [],
+    headers: [
+      {
+        text: "Article Number",
+        align: "start",
+        value: "firma"
+      },
+      { text: "Name", value: "docu-nr" },
+      { text: "Minimum On Hand", value: "traubensort" },
+      { text: "Current On Hand", value: "lief-einheit" },
+      { text: "Average Price", value: "betriebsnr" },
+      { text: "Actual Price", value: "anzahl" },
+      { text: "Last Purchase Date", value: "einzelpreis" }
+    ]
   })
 };
 </script>
