8603aecad36e9c82ec6018b924f7c4da615c15e3
dedychaidir <dedy.chaidir@gmail.com>
2020-03-17 16:40:52 +0700

    test

---
+++ src/vhp-modules/outlet/reports/tes-report/utils/getDataPrepare.vue
@@ -0,0 +1,35 @@
+<script>
+import ky from "ky";
+export default {
+  data: () => ({
+    dataPrepare: []
+  }),
+  mounted: {
+    writeDataPrepare() {
+      (async () => {
+        const parsed = await ky
+          .post(
+            "http://ws1.e1-vhp.com/VHPWebBased/rest/vhpINV/slowMovingList",
+            {
+              json: {
+                request: {
+                  inputUserkey: "6D83EFC6F6CA694FFC35FAA7D70AD308FB74A6CD",
+                  inputUsername: "sindata",
+                  storeNo: "1",
+                  mainGrp: "1",
+                  tage: "0",
+                  showPrice: "true"
+                }
+              }
+            }
+          )
+          .json();
+        // const pbookList = parsed.response.sList["s-list"];
+        console.log(parsed, "data");
+
+        // this.datas = pbookList;
+      })();
+    }
+  }
+};
+</script>
