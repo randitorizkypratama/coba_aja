ed73da25e54e7b51754ea3716a1070e200aee8d8
anitalim <anita.lim@hotmail.com>
2020-03-19 16:07:44 +0700

    s

--- utils/api/useFetchCommon.ts
+++ utils/api/useFetchCommon.ts
@@ -2,13 +2,16 @@
 import { FETCH_COMMON } from "./api";
 import ky from "ky";
 
-export default function name(api: string, additionalParameters: object) {
+export default function name(
+  api: string,
+  additionalParameters: object
+): object {
   const credentials: object = {
     inputUsername: "sindata",
     inputUserkey: "6D83EFC6F6CA694FFC35FAA7D70AD308FB74A6CD"
   };
   const params: object = Object.assign(credentials, additionalParameters);
-  async function ambilData() {
+  async function ambilData(): Promise<any> {
     const data = await ky
       .post(FETCH_COMMON + api, {
         json: {
