2141270242b703d0491a2df8c99b17079f880a2e
cybanjar <cybanjar@gmail.com>
2020-05-06 19:08:25 +0700

    commit quasar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,6 @@
 import Vue from 'vue';
 import VueRouter, { Route } from 'vue-router';
 
-<<<<<<< HEAD
-import storeModule from '~/store';
-import routes from './routes';
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  scrollBehavior: () => ({ x: 0, y: 0 }),
-  routes,
-
-  // Leave these as they are and change in quasar.conf.js instead!
-  // quasar.conf.js -> build -> vueRouterMode
-  // quasar.conf.js -> build -> publicPath
-  mode: process.env.VUE_ROUTER_MODE,
-  base: process.env.VUE_ROUTER_BASE,
-});
-
-// this function is to change the contents of the submodule menu
-const changeModule = (to: Route) => {
-  const { SET_CURRENT_MODULE } = storeModule('layout').useMutations([
-    'SET_CURRENT_MODULE',
-  ]);
-
-  if (to.meta.module) {
-    SET_CURRENT_MODULE(to.meta.module);
-  } else {
-    SET_CURRENT_MODULE(null);
-=======
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
@@ -241,7 +213,6 @@ const routes = [
     path: "/cancelled-incoming",
     name: "Cancelled Incoming",
     component: CancelledIncoming
->>>>>>> 8f983e9411db9636e390d6371044465f01d24dcb
   }
 };
 
