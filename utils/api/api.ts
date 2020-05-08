cc17fb2b7cf6c277f32c2b1a20c338e9bdc8de13
cybanjar <cybanjar@gmail.com>
2020-05-08 09:45:00 +0700

    First Git Clone

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -1,8 +1,8 @@
 /** @format */
-import { modul } from "./config";
-export const API_URL = modul.HTTP + modul.WEB_SERVICE + modul.URL + "vhpINV/";
+import { modul } from './config';
+export const API_URL = modul.HTTP + modul.WEB_SERVICE + modul.URL + 'vhpINV/';
 export const API_LOGIN =
-  "http://54.251.169.160:8080/logserver/rest/loginServer/";
+  'http://54.251.169.160:8080/logserver/rest/loginServer/';
 
-export const FETCH_COMMON = "http://ws1.e1-vhp.com/VHPWebBased/rest/";
+export const FETCH_COMMON = 'http://ws1.e1-vhp.com/VHPWebBased/rest/';
 export default { API_URL, API_LOGIN, FETCH_COMMON };
